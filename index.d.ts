bcb7ee93fe951b96d19a0b49216512fce430ea8f
ufukbakan <println.ufukbakan@gmail.com>
2023-02-13 10:19:26 +0300

    update type declaration

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
 import { RequestHandler } from "express"
 
-class HttpError extends Error {
+declare class HttpError extends Error {
     status: number
     message: string
-    constructor(params: { status: number, message: string }): void;
+    constructor(params: { status: number, message: string })
 }
 
 type RestedService<T extends Record<string, any>> = { [K in keyof T]:
@@ -12,9 +12,9 @@ type RestedService<T extends Record<string, any>> = { [K in keyof T]:
     : T[K]
 }
 
-function RestService<T extends Record<string, any>>(service: T): RestedService<T>;
-function RestMethod<T>(callback: () => T): RequestHandler;
-function Restify(target: any, key: string, desc: PropertyDescriptor): PropertyDescriptor;
+declare function RestService<T extends Record<string, any>>(service: T): RestedService<T>;
+declare function RestMethod<T>(callback: () => T): RequestHandler;
+declare function Restify(target: any, key: string, desc: PropertyDescriptor): PropertyDescriptor;
 
 export {
     HttpError,
