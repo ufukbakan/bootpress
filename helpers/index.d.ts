99fd9f72b03c8077904e8d13df3769946a49edb1
Ufuk Bakan <println.ufukbakan@gmail.com>
2023-02-26 00:30:02 +0300

    add validator helpers

--- helpers/index.d.ts
+++ helpers/index.d.ts
@@ -1,4 +1,20 @@
 import { HttpError } from "..";
 
+type JsSchema = {
+    [key: string]: "string" | "boolean" | "number" | JsSchema
+}
+
+type TypeMap = {
+    "string": string,
+    "boolean": boolean,
+    "number": number,
+}
+
+type SchemadRecord<T> = { [E in keyof T]: T[E] extends string ? TypeMap[T[E]] : SchemadRecord<T[E]> };
+
 export function getOrThrow<T, E extends HttpError>(data: T, error: E): T;
 export function getOrElse<T, E>(data: T, defaultValue: E): T | E;
+export function asBoolean(o: any): boolean;
+export function asNumber(o: any): number;
+export function asInteger(o: any): number;
+export function asSchema<T extends JsSchema>(o: any, jsSchema: T): SchemadRecord<T>
--- helpers/index.js
+++ helpers/index.js
@@ -1,20 +1,92 @@
-function getOrThrow(data, error){
-    if(data === null || data === undefined){
+const { HttpError } = require("..");
+
+function getOrThrow(data, error) {
+    if (data === null || data === undefined) {
         throw error;
-    }else{
+    } else {
         return data;
     }
 }
 
-function getOrElse(data, defaultValue){
-    if(data === null || data === undefined){
+function getOrElse(data, defaultValue) {
+    if (data === null || data === undefined) {
         return defaultValue;
-    }else{
+    } else {
         return data;
     }
+}
+
+function asBoolean(o, errorMessage = undefined, errorStatus = 400) {
+    errorMessage = errorMessage ?? `Value ${o} should have been a boolean but it is not.`;
+    if (typeof o === "string") {
+        const lowercased = o.toLowerCase();
+        const validBooleanStrings = new Map(Object.entries({
+            "true": true,
+            "false": false,
+            "1": true,
+            "0": false,
+            "yes": true,
+            "no": false
+        }));
+        if (!validBooleanStrings.has(lowercased)) {
+            throw new HttpError(errorStatus, errorMessage);
+        }
+        return validBooleanStrings.get(lowercased);
+    } else if (typeof o === "boolean") {
+        return o;
+    }
+    throw new HttpError(errorStatus, errorMessage);
+}
+
+function asNumber(o, errorMessage = undefined, errorStatus = 400) {
+    errorMessage = errorMessage ?? `Value ${o} should have been a number but it is not.`
+    if (typeof o === "number") {
+        return o;
+    }
+    else if (typeof o === "string") {
+        const castedValue = Number(o);
+        if (isNaN(castedValue)) {
+            throw new HttpError(errorStatus, errorMessage);
+        }
+    }
+    throw new HttpError(errorStatus, errorMessage);
+}
+
+function asInteger(o, errorMessage = undefined, errorStatus = 400) {
+    errorMessage = errorMessage ?? `Value ${o} should have been a integer but it is not.`;
+    let value = o;
+    if (typeof o === "string") {
+        value = Number(o);
+    }
+    if (!Number.isInteger(value)) {
+        throw new HttpError(errorStatus, errorMessage);
+    }
+    return value;
+}
+
+function asSchema(o, schema){
+    const schemaKeyValues = Object.entries(schema);
+    for(let i = 0; i < schemaKeyValues.length; i ++){
+        const key = schemaKeyValues[i][0];
+        const expectedType = schemaKeyValues[i][1];
+        if(typeof expectedType === "object"){
+            asSchema(o[key], expectedType);
+        }else if(typeof expectedType === "string"){
+            if(typeof o[key] !== expectedType){
+                throw new HttpError(400, `Value of ${key} should have been a ${expectedType} but it is a ${typeof o[key]}`);
+            }
+        }else {
+            throw new HttpError(500, `Type of a schema key should be a primitive type or another schema`);
+        }
+    }
+    return o;
 }
 
 module.exports = {
     getOrThrow,
-    getOrElse
+    getOrElse,
+    asBoolean,
+    asNumber,
+    asInteger,
+    asSchema
 }
